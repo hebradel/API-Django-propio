aad80dd1eeee804b0391fbcad6dcf98c328e51e7
hebradel <hebradel@gmail.com>
2025-05-12 17:00:14 +0000

    alpha 1

---
+++ cuenta.py
@@ -0,0 +1,10 @@
+salir=1
+while salir!=3:
+    print('\n 1.Crear cuenta: \n 2.Eliminar cuenta: \n 3.Salir: ')
+    salir=int(input('\nSeleccione una opcion: '))
+    if salir==1:
+        print('\ncreando cuenta .....\n')
+    elif salir==2:
+        print('\nEliminando cuenta .....\n')
+    elif salir==3:
+        print('\nSaliendo .....\n')
---
+++ suma_ciclo.py
@@ -0,0 +1,7 @@
+maximo =5
+minimo =1
+acumulador =0
+while minimo<=maximo:
+    acumulador+=minimo
+    minimo+=1
+print(f'Numero  {acumulador}')
